9ee3c5b493565966be0ab4fa517c4d22b7a7ef70
anthony-devarti <anthony.devarti@gmail.com>
2022-10-26 21:50:48 -0400

    set up routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './routes/Home'
+import Users from './routes/Users'
+import About from './routes/About'
+import { Navbar, Nav, Container } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Trashmasters Web App
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navbar bg="dark" variant="dark">
+          <Container>
+            <Navbar.Brand>
+              <Link to="/">
+                The Goblin TrashMasters
+              </Link>
+            </Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link href="#home">
+                <Link to="/">
+                  Home
+                </Link>
+              </Nav.Link>
+              <Nav.Link>
+                <Link to="/about">
+                  About
+                </Link>
+              </Nav.Link>
+              <Nav.Link>
+                <Link to="/users">
+                  Users
+                </Link>
+              </Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+        <Navbar>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </Navbar>
+
+        <Routes>
+          <Route path="/about" element={<About />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import { Navbar } from "react-bootstrap"
+
+export default function Navbar() {
+    return (
+        <>
+        <Navbar>
+            
+        </Navbar>
+        </>
+    )
+}
