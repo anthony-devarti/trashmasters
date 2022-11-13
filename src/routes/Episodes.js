2a627dc13bf0e0e287226c0878d035d7415f3a1f
anthony-devarti <anthony.devarti@gmail.com>
2022-11-13 14:28:30 -0500

    more fixes

--- src/routes/Episodes.js
+++ src/routes/Episodes.js
@@ -1,6 +1,40 @@
 import EpisodeCard from "../components/EpisodeCard";
+import { useEffect, useState } from "react";
+import { useFeed } from "react-hook-rss";
+import { rssUrl } from "../data/appConstants";
 
-export default function Episodes() {
+export default function Episodes(props) {
+
+
+    // const feed = useFeed("https://brainstormbrewery.com/category/podcasts/brainstormbrewery/feed/");
+
+    // useEffect(() => {
+    //     console.log("the feed", feed);
+    // }, [feed]);
+
+    const [ feed, setFeed ] = useState([])
+    console.log("feed", feed)
+
+    useEffect(() => {
+
+        fetch(rssUrl).
+            then(response => response.text).
+            then(str => {
+                const parser = new window.DOMParser();
+                const data = parser.parseFromString(str, 'text/xml');
+                const itemList = data.querySelectorAll('item');
+                const items = [];
+                itemList.forEach((el) => {
+                    items.push({
+                        title: el.querySelector('title').innerHTML,
+                        pubDate: new Date(el.querySelector('pubdate').textContent),
+                        mp3: el.querySelector('enclosure').getAttribute('url')
+                    })
+                })
+                setFeed(items);
+            })
+    },[])
+
 
     //probably will need some sort of useEffect to go grab the podcast episodes from the backend.
 
@@ -15,9 +49,9 @@ export default function Episodes() {
 
     return (
         <>
-        Episodes
-        
-        <EpisodeCard episode={exampleEpisode}/>
+            Episodes
+
+            <EpisodeCard episode={exampleEpisode} />
         </>
     )
 }
