5d6e6b38e2e1ca84cec214cd7e242891aca19ef8
anthony-devarti <anthony.devarti@gmail.com>
2022-12-08 11:20:48 -0500

    this is pretty fucked,  I think I'm going to abandon this branch

--- src/routes/Episodes.js
+++ src/routes/Episodes.js
@@ -1,52 +1,23 @@
 import EpisodeCard from "../components/EpisodeCard";
-import { useEffect, useState } from "react";
-import { rssUrl } from "../data/appConstants";
 
-export default function Episodes(props) {
+export default function Episodes() {
 
-    const [rowData, setRowData] = useState()
+    //probably will need some sort of useEffect to go grab the podcast episodes from the backend.
 
-    useEffect(() => {
-
-        fetch(rssUrl)
-            .then(response => response.text())
-            .then(str => new window.DOMParser().parseFromString(str, 'text/xml'))
-            .then(data => {
-                console.log("data in the request", data)
-                const itemList = data.querySelectorAll('item');
-
-                const items = [];
-                itemList.forEach(el => {
-                    items.push({
-                        pubDate: new Date(el.querySelector('pubDate').textContent),
-                        title: el.querySelector('title').innerHTML,
-                        mp3: el.querySelector('enclosure').getAttribute('url'),
-                        description: el.querySelector('description').textContent,
-                        link: el.querySelector('link').textContent,
-                    });
-                });
-
-                setRowData(items)
-            });
-
-    }, []);
+    const exampleEpisode = {
+        title: 'Example Title',
+        description: 'Here is an example description',
+        release: '10/31/22',
+        image: "https://i.imgur.com/ckSgzLQ.png",
+        // guestName: "Me",
+        // guestInfo: "I'm just a little guy."
+    }
 
     return (
         <>
-            <h1 className="title-section center">
-                Episodes
-            </h1>
-            <h2 className="center">Check out all of our episodes right here.</h2>
-            {
-                rowData &&
-                rowData.length > 0 &&
-                rowData.map((row, index) => {
-                    return (
-                        <EpisodeCard episode={row} />
-                    )
-                })
-            }
-            {/* <EpisodeCard episode={rowData[0]} /> */}
+        Episodes
+        
+        <EpisodeCard episode={exampleEpisode}/>
         </>
     )
 }
