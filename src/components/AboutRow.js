f24528a5a6b8959717ed78a83371a096378d04fe
anthony-devarti <anthony.devarti@gmail.com>
2022-12-28 01:13:33 -0500

    fixed about rows

--- src/components/AboutRow.js
+++ src/components/AboutRow.js
@@ -11,32 +11,30 @@ image: path to image
 export default function AboutRow(props) {
     return (
         <Row>
-            <Col>
-                <div className="skew-col name">
-                    <div className="unskewed name-text">
-                        {props.name}
-                    </div>
+            <div className="skew-col name">
+                <div className="unskewed name-text">
+                    {props.name}
                 </div>
-                <div className="skew-col info">
-                    <div className="unskewed text">
-                        <ul>
-                            {props.info.map((point) => {
-                                return (
-                                    <li key={point}>{point}</li>
-                                )
-                            })}
-                        </ul>
-                    </div>
+            </div>
+            <div className="skew-col info">
+                <div className="unskewed text">
+                    <ul>
+                        {props.info.map((point) => {
+                            return (
+                                <li key={point}>{point}</li>
+                            )
+                        })}
+                    </ul>
                 </div>
-                <div className="skew-col roles">
-                    <div className="unskewed text">
-                        {props.roles}
-                    </div>
+            </div>
+            <div className="skew-col roles">
+                <div className="unskewed text">
+                    {props.roles}
                 </div>
-                <div className='picture'>
-                    A picture goes here
-                </div>
-            </Col>
+            </div>
+            <div className='picture'>
+                A picture goes here
+            </div>
         </Row>
     )
 }
