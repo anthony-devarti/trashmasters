9f37c297fde3ff4cb45015725349e2bd8ab1811d
anthony-devarti <anthony.devarti@gmail.com>
2022-12-31 02:03:58 -0500

    add 404 page

--- src/routes/NotFound.js
+++ src/routes/NotFound.js
@@ -1,7 +1,30 @@
-export default function NotFound () {
-    return(
-        <>
-        The requested route could not be found.  Click a link in the navbar to go back to the site.
-        </>
+import { Button, Container, Row, Col } from 'react-bootstrap'
+import ErrorLogo from '../img/ErrorLogo.jpg'
+import { Link } from 'react-router-dom'
+
+export default function NotFound() {
+    return (
+        <Container>
+            <Row>
+                <Col className='title-section'>
+                    404 Page Not Found
+                </Col>
+            </Row>
+            <Row>
+                <Col style={{ display: 'flex', justifyContent: 'center' }}>
+                    <img src={ErrorLogo} style={{ height: '50vh' }} />
+                </Col>
+            </Row>
+            <Row>
+                <Col style={{ display: 'flex', justifyContent: 'center' }}>
+                    <Link
+                        to="/"
+                        className='custom-button custom-hover'
+                    >
+                        Go Home
+                    </Link>
+                </Col>
+            </Row>
+        </Container>
     )
 }
